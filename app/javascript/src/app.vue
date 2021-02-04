efb6fcd5763a0a317f2d5937fad7faec2ef58a7e
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-02-04 16:48:06 +0900

    add vue-cookies

--- app/javascript/src/app.vue
+++ app/javascript/src/app.vue
@@ -1,18 +1,61 @@
 <template>
-  <router-view />
+  <router-view></router-view>
 </template>
 
 <script>
+import Vue from 'vue'
+
+/* Vue Router */
 import router from './router'
+
+/* Vue Cookies */
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
+Vue.$cookies.config('7d')
+
+/* Vue Modal */
+import VModal from 'vue-js-modal'
+Vue.use(VModal)
+
+/* Vue draggable */
+import draggable from 'vuedraggable'
+Vue.component('draggable', draggable)
+
+/* Bootstrap */
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+/* fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faEdit, faCheck, faTrash, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faUser, faEdit, faCheck, faTrash, faTimesCircle)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 export default {
   router
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
+<style>
+body {
+  background: snow;
+}
+.btn:focus {
+  box-shadow: none !important;
+}
+.vm--modal {
+  top: 5% !important;
+  left: 0 !important;
+  height: auto !important;
+  margin: 0 auto;
+  padding: 2rem;
+  background: aliceblue;
+  border-radius: 10px;
+}
+.vm--modal h3 {
   text-align: center;
 }
 </style>
