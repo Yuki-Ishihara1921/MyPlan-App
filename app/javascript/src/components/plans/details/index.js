ddc2b4eb80c1c35cf4ba17fe2cff521bb77dc2a0
Yuki-Ishihara1921 <1921cd.p@gmail.com>
2021-03-04 10:55:42 +0900

    change fileName sort

--- app/javascript/src/components/plans/details/index.js
+++ app/javascript/src/components/plans/details/index.js
@@ -1,3 +1,3 @@
+export { default as DetailButtons } from './DetailButtons'
 export { default as DetailHeader } from './DetailHeader'
 export { default as DetailTable } from './DetailTable'
-export { default as DetailButtons } from './DetailButtons'
--- app/javascript/src/components/plans/outlines/index.js
+++ app/javascript/src/components/plans/outlines/index.js
@@ -1,3 +1,3 @@
+export { default as OutlineButtons } from './OutlineButtons'
 export { default as OutlineHeader } from './OutlineHeader'
 export { default as OutlineTable } from './OutlineTable'
-export { default as OutlineButtons } from './OutlineButtons'
